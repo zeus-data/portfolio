58f789089c23b07c0c3f50e3eb1efa92954072f0
Dimitri_Papadopoulos <dimitri.mba@gmail.com>
2024-12-15 01:24:44 +0000

    fixed a typo

--- views/About_me.py
+++ views/About_me.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Welcome to Zeus-Data's Projects by Dimitri")
 st.write("On the left sidebar, you may discover an interesting project to explore.")
-st.write('(Click the `>` icon on the top-right to expand the sidebar)')
+st.write('(Click the `>` icon on the top-left to expand the sidebar)')
 
 st.image('zeus_athens.png')
