4513048ebe7889fe59964c70fe348eb1b7f9a63e
Dimitri_Papadopoulos <dimitri.mba@gmail.com>
2024-12-15 01:14:55 +0000

    Edit on the About Me page

--- views/About_me.py
+++ views/About_me.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Welcome to Zeus-Data's Projects")
+st.title("Welcome to Zeus-Data's Projects by Dimitri")
 st.write("On the left sidebar, you may discover an interesting project to explore.")
 
 st.image('zeus_athens.png')
