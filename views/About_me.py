76c7e1d46cc58d021a43937d9cc06f68e2e234ad
Dimitri_Papadopoulos <dimitri.mba@gmail.com>
2024-12-15 01:23:46 +0000

    Added a tip to expand the left sidebar

--- views/About_me.py
+++ views/About_me.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("Welcome to Zeus-Data's Projects by Dimitri")
 st.write("On the left sidebar, you may discover an interesting project to explore.")
+st.write('(Click the `>` icon on the top-right to expand the sidebar)')
 
 st.image('zeus_athens.png')
